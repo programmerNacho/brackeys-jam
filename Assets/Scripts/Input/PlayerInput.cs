3e5198ace7b3bada623a7009cd7b025ba611196c
Lobo-Iberico <Lobo-Iberico@users.noreply.github.com>
2021-02-15 22:37:24 +0100

    Movement and Rotate player

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private ShipController shipController = null;
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    private void Start()
     {
-        
+        SerializeVariables();
+    }
+    private void SerializeVariables()
+    {
+        if (shipController == null) shipController = GetComponent<ShipController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        LookAtMouse();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(x, y);
+        print(direction);
+
+        shipController.MoveTowardsDirectionAcceleration(direction);
+    }
+    private void LookAtMouse()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector2 mousePosition = Input.mousePosition;
+            Vector2 centerScreen = new Vector2(Screen.width / 2, Screen.height / 2);
+
+            Vector2 targetLookDirection = (mousePosition - centerScreen).normalized;
+
+            shipController.RotateTowardDirectionAcceleration(targetLookDirection);
+        }
     }
 }
