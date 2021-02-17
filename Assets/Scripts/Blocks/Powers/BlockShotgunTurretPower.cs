2f75c8d8ce30546baf889e0c544ab06352f2f04a
programmerNacho <56942092+programmerNacho@users.noreply.github.com>
2021-02-17 19:59:48 +0100

    All Prefabs prepared

---
+++ Assets/Scripts/Blocks/Powers/BlockShotgunTurretPower.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class BlockShotgunTurretPower : BlockTurretPower
+    {
+        public override void OnBlockConnected()
+        {
+
+        }
+
+        public override void OnBlockDisconnected()
+        {
+
+        }
+
+        public override void AddBoost()
+        {
+            base.AddBoost();
+        }
+
+        public override void RemoveBoost()
+        {
+            base.RemoveBoost();
+        }
+    }
+}
---
+++ Assets/Scripts/Blocks/Powers/BlockSimpleTurretPower.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class BlockSimpleTurretPower : BlockTurretPower
+    {
+        public override void OnBlockConnected()
+        {
+
+        }
+
+        public override void OnBlockDisconnected()
+        {
+
+        }
+
+        public override void AddBoost()
+        {
+            base.AddBoost();
+        }
+
+        public override void RemoveBoost()
+        {
+            base.RemoveBoost();
+        }
+    }
+}
