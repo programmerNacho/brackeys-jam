8af4567c7901cdf8718a38f460c27a4b20a63a80
programmerNacho <56942092+programmerNacho@users.noreply.github.com>
2021-02-19 17:03:22 +0100

    Zones completed

---
+++ Assets/Scripts/Zones/Zone.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Zone : MonoBehaviour
+    {
+        [SerializeField]
+        private float maxDistance = 200f;
+
+        private List<Transform> children = new List<Transform>();
+
+        private void Start()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                if(child.gameObject.activeSelf == true)
+                {
+                    children.Add(child);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            List<Transform> childsToSeparate = new List<Transform>();
+
+            foreach (Transform child in children)
+            {
+                float distanceToZone = Vector2.Distance(child.position, transform.position);
+
+                if(distanceToZone >= maxDistance)
+                {
+                    childsToSeparate.Add(child);
+                }
+            }
+
+            foreach (Transform child in childsToSeparate)
+            {
+                children.Remove(child);
+
+                if(child.parent == transform)
+                {
+                    child.parent = null;
+                }
+            }
+
+            if(children.Count <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
